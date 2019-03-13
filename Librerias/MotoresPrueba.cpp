5009fbc60f1583cda9c308da8b3bc30dd4d14571
Waxymantis <43016726+Waxymantis@users.noreply.github.com>
2019-03-12 20:53:07 -0600

    Add files via upload

---
+++ Librerias/MotoresPrueba.cpp
@@ -0,0 +1,257 @@
+ #include "MotoresPrueba.h"
+
+double Kp=2, Ki=0, Kd=0;
+double Input=0, Output=0, Setpoint=0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, REVERSE);
+
+
+MotoresPrueba::MotoresPrueba(){
+bno = Adafruit_BNO055();
+AFMS = Adafruit_MotorShield();
+MotorAdeIzq = AFMS.getMotor(4);
+MotorAdeDer = AFMS.getMotor(1);
+MotorAtrasIzq = AFMS.getMotor(3);
+MotorAtrasDer = AFMS.getMotor(2);
+}
+
+void MotoresPrueba::setup(){
+    AFMS.begin();
+    if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+   delay(1000);
+  bno.setExtCrystalUse(true);
+  Input;
+  Setpoint;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+}
+
+void MotoresPrueba::moveAdelante(){
+    uint8_t i;
+
+    MotorAtrasDer->setSpeed(250);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(250);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(250);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(250);
+    MotorAdeDer->run(BACKWARD);
+}
+void MotoresPrueba::moveAdelanteLento(){
+    uint8_t i;
+
+    MotorAtrasDer->setSpeed(50);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(50);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(50);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(50);
+    MotorAdeDer->run(BACKWARD);
+}
+void MotoresPrueba::acomodoD()
+{
+  uint8_t i;
+
+    MotorAtrasDer->setSpeed(170);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(50);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(50);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(170);
+    MotorAdeDer->run(BACKWARD);
+}
+void MotoresPrueba::acomodoI()
+{
+  uint8_t i;
+
+  MotorAtrasDer->setSpeed(50);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(170);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(170);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(50);
+    MotorAdeDer->run(BACKWARD);
+}
+void MotoresPrueba::detenerse(){
+  MotorAdeIzq->run(RELEASE);
+  MotorAdeDer->run(RELEASE);
+  MotorAtrasIzq->run(RELEASE);
+  MotorAtrasDer->run(RELEASE);
+}
+void MotoresPrueba::moveAtras(){
+uint8_t i;
+
+    MotorAtrasDer->setSpeed(250);
+    MotorAtrasDer->run(BACKWARD);
+    MotorAtrasIzq->setSpeed(250);
+    MotorAtrasIzq->run(BACKWARD);
+    MotorAdeIzq->setSpeed(250);
+    MotorAdeIzq->run(FORWARD);
+    MotorAdeDer->setSpeed(250);
+    MotorAdeDer->run(FORWARD);
+}
+void MotoresPrueba::moveAtrasLento(){
+uint8_t i;
+
+    MotorAtrasDer->setSpeed(50);
+    MotorAtrasDer->run(BACKWARD);
+    MotorAtrasIzq->setSpeed(50);
+    MotorAtrasIzq->run(BACKWARD);
+    MotorAdeIzq->setSpeed(50);
+    MotorAdeIzq->run(FORWARD);
+    MotorAdeDer->setSpeed(50);
+    MotorAdeDer->run(FORWARD);
+}
+void MotoresPrueba::moveIzq(){
+uint8_t i;
+double med=0, newMed=0, punto=0, newPunto=0;
+
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    med=euler.x();
+    Serial.println(med);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+
+if(med<90){
+
+    punto=med-85;
+    newPunto=360+punto;
+    Setpoint=newPunto;
+    do{
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    newMed=euler.x();
+    Input=newMed;
+    Serial.println(newMed);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+    myPID.Compute();
+    Output+=100;
+    (Output>255)? Output=255: Output=Output;
+    Serial.println(Output);
+    //analogWrite(PIN_OUTPUT, Output);
+    MotorAtrasDer->setSpeed(Output);
+    MotorAtrasDer->run(BACKWARD);
+    MotorAtrasIzq->setSpeed(Output);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(Output);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(Output);
+    MotorAdeDer->run(FORWARD);
+
+    }while(newMed<=med || newMed>newPunto);
+  }
+  else{
+    punto=med-85;
+    Setpoint=punto;
+    do{
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    newMed=euler.x();
+    Input=newMed;
+    Serial.println(newMed);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+    myPID.Compute();
+    Output+=100;
+    (Output>255)? Output=255: Output=Output;
+    Serial.println(Output);
+    //analogWrite(PIN_OUTPUT, Output);
+    MotorAtrasDer->setSpeed(Output);
+    MotorAtrasDer->run(BACKWARD);
+    MotorAtrasIzq->setSpeed(Output);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(Output);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(Output);
+    MotorAdeDer->run(FORWARD);
+    }while(newMed>punto);
+  }
+  Serial.println("YA SALIO********");
+  MotorAdeIzq->run(RELEASE);
+  MotorAdeDer->run(RELEASE);
+  MotorAtrasIzq->run(RELEASE);
+  MotorAtrasDer->run(RELEASE);
+}
+
+void MotoresPrueba::moveDer(){
+uint8_t i;
+double med=0, newMed=0, punto=0, newPunto=0;
+
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    med=euler.x();
+    Serial.println(med);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+
+    if(med>=270){
+
+    punto=med+90;
+    newPunto=(360-punto)*-1;
+    do{
+    MotorAtrasDer->setSpeed(250);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(250);
+    MotorAtrasIzq->run(BACKWARD);
+    MotorAdeIzq->setSpeed(250);
+    MotorAdeIzq->run(FORWARD);
+    MotorAdeDer->setSpeed(250);
+    MotorAdeDer->run(BACKWARD);
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    newMed=euler.x();
+    Serial.println(newMed);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+    }while(newMed>=med || newMed<newPunto);
+  }
+  else{
+    punto=med+90;
+    do{
+    MotorAtrasDer->setSpeed(250);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(250);
+    MotorAtrasIzq->run(BACKWARD);
+    MotorAdeIzq->setSpeed(250);
+    MotorAdeIzq->run(FORWARD);
+    MotorAdeDer->setSpeed(250);
+    MotorAdeDer->run(BACKWARD);
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    newMed=euler.x();
+    Serial.println(newMed);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+    }while(newMed<punto);
+  }
+  Serial.println("YA SALIO********");
+  MotorAdeIzq->run(RELEASE);
+  MotorAdeDer->run(RELEASE);
+  MotorAtrasIzq->run(RELEASE);
+  MotorAtrasDer->run(RELEASE);
+}
+void MotoresPrueba::moveDerAcomodo(){
+   MotorAtrasDer->setSpeed(80);
+    MotorAtrasDer->run(FORWARD);
+    MotorAtrasIzq->setSpeed(80);
+    MotorAtrasIzq->run(BACKWARD);
+    MotorAdeIzq->setSpeed(80);
+    MotorAdeIzq->run(FORWARD);
+    MotorAdeDer->setSpeed(80);
+    MotorAdeDer->run(BACKWARD);
+}
+void MotoresPrueba::moveIzqAcomodo(){
+    MotorAtrasDer->setSpeed(80);
+    MotorAtrasDer->run(BACKWARD);
+    MotorAtrasIzq->setSpeed(80);
+    MotorAtrasIzq->run(FORWARD);
+    MotorAdeIzq->setSpeed(80);
+    MotorAdeIzq->run(BACKWARD);
+    MotorAdeDer->setSpeed(80);
+    MotorAdeDer->run(FORWARD);
+}
