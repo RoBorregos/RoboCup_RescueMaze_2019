39f7bd47211cea47ab37c5a8cc0b2701cb7360d3
Waxymantis <43016726+Waxymantis@users.noreply.github.com>
2019-03-17 17:12:00 -0600

    Update Rampa.cpp

--- Librerias/Rampa/Rampa.cpp
+++ Librerias/Rampa/Rampa.cpp
@@ -1,19 +1,14 @@
-#include "Rampa.h"
+  #include "Rampa.h"
 
 
 
 
 Rampa::Rampa(){
 bno = Adafruit_BNO055();
-AFMS = Adafruit_MotorShield();
-MotorAdeIzq = AFMS.getMotor(4);
-MotorAdeDer = AFMS.getMotor(1);
-MotorAtrasIzq = AFMS.getMotor(3);
-MotorAtrasDer = AFMS.getMotor(2);
 }
 
 void Rampa::setup(){
-    AFMS.begin();
+
     if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -35,65 +30,73 @@ double med=0, newMed=0, punto=0, newPunto=0;
     Serial.println(med);
     delay(BNO055_SAMPLERATE_DELAY_MS);
 
- if(med<-1){
+ if(med<-3){
     Serial.println("Entramos");
     do{
-    MotorAtrasDer->setSpeed(150);
-    MotorAtrasDer->run(FORWARD);
-    MotorAtrasIzq->setSpeed(150);
-    MotorAtrasIzq->run(FORWARD);
-    MotorAdeIzq->setSpeed(150);
-    MotorAdeIzq->run(BACKWARD);
-    MotorAdeDer->setSpeed(150);
-    MotorAdeDer->run(BACKWARD);
+            digitalWrite(motorIzqAde1, LOW);
+            analogWrite(motorIzqAde2, 220);
+            digitalWrite(motorIzqAtras1, LOW);
+            analogWrite(motorIzqAtras2, 220);
+            digitalWrite(motorDerAde1, LOW);
+            analogWrite(motorDerAde2, 255);
+            digitalWrite(motorDerAtras1, LOW);
+            analogWrite(motorDerAtras2, 255);
     imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
     Serial.print("X: ");
     newMed=euler.y();
+    if(newMed>-1){
+        newMed=-5;
+    }
     Serial.println(newMed);
     delay(BNO055_SAMPLERATE_DELAY_MS);
-    }while(newMed<-2);
+    }while(newMed<-3);
     Serial.println("Salimos");
+            digitalWrite(motorIzqAde1, LOW);
+            analogWrite(motorIzqAde2, 200);
+            digitalWrite(motorIzqAtras1, LOW);
+            analogWrite(motorIzqAtras2, 200);
+            digitalWrite(motorDerAde1, LOW);
+            analogWrite(motorDerAde2, 255);
+            digitalWrite(motorDerAtras1, LOW);
+            analogWrite(motorDerAtras2, 255);
+            delay(150);
     valor++;
     }
     else if(med>8){
     Serial.println("Entramos");
-    MotorAtrasDer->setSpeed(235);
-    MotorAtrasDer->run(BACKWARD);
-    MotorAtrasIzq->setSpeed(255);
-    MotorAtrasIzq->run(BACKWARD);
-    MotorAdeIzq->setSpeed(255);
-    MotorAdeIzq->run(FORWARD);
-    MotorAdeDer->setSpeed(235);
-    MotorAdeDer->run(FORWARD);
-    delay(1000);
-    MotorAtrasDer->setSpeed(235);
-    MotorAtrasDer->run(FORWARD);
-    MotorAtrasIzq->setSpeed(255);
-    MotorAtrasIzq->run(FORWARD);
-    MotorAdeIzq->setSpeed(255);
-    MotorAdeIzq->run(BACKWARD);
-    MotorAdeDer->setSpeed(235);
-    MotorAdeDer->run(BACKWARD);
-    delay(1200);
+
     do{
-    MotorAtrasDer->setSpeed(235);
-    MotorAtrasDer->run(FORWARD);
-    MotorAtrasIzq->setSpeed(255);
-    MotorAtrasIzq->run(FORWARD);
-    MotorAdeIzq->setSpeed(255);
-    MotorAdeIzq->run(BACKWARD);
-    MotorAdeDer->setSpeed(235);
-    MotorAdeDer->run(BACKWARD);
+
+            digitalWrite(motorIzqAde1, LOW);
+            analogWrite(motorIzqAde2, 130);
+            digitalWrite(motorIzqAtras1, LOW);
+            analogWrite(motorIzqAtras2, 130);
+            digitalWrite(motorDerAde1, LOW);
+            analogWrite(motorDerAde2, 160);
+            digitalWrite(motorDerAtras1, LOW);
+            analogWrite(motorDerAtras2, 160);
     imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
     Serial.print("X: ");
     newMed=euler.y();
+    if(newMed<4){
+        newMed=10;
+    }
     Serial.println(newMed);
     delay(BNO055_SAMPLERATE_DELAY_MS);
     }while(newMed>8);
     Serial.println("Salimos");
+            digitalWrite(motorIzqAde1, LOW);
+            analogWrite(motorIzqAde2, 200);
+            digitalWrite(motorIzqAtras1, LOW);
+            analogWrite(motorIzqAtras2, 200);
+            digitalWrite(motorDerAde1, LOW);
+            analogWrite(motorDerAde2, 255);
+            digitalWrite(motorDerAtras1, LOW);
+            analogWrite(motorDerAtras2, 255);
+            delay(150);
 
     valor++;
     }
+
     return valor;
 }
-
